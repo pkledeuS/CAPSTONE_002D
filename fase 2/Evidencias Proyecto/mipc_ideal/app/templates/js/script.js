5ed65d902edfd6abfea87fa7c031088ecac943be
carlosncho <caremisaafa@gmail.com>
2025-09-22 17:53:43 -0300

    confi de boton de "perfil"

--- fase 2/Evidencias Proyecto/mipc_ideal/app/templates/js/script.js
+++ fase 2/Evidencias Proyecto/mipc_ideal/app/templates/js/script.js
@@ -1,3 +1,23 @@
+// menú desplegable
+const menuToggle = document.getElementById("menuToggle");
+const menuOptions = document.getElementById("menuOptions");
+
+menuToggle.addEventListener("click", () => {
+  menuOptions.style.display =
+    menuOptions.style.display === "block" ? "none" : "block";
+});
+
+// Cierra el menú si haces clic fuera de él
+window.addEventListener("click", (event) => {
+  if (!menuToggle.contains(event.target) && !menuOptions.contains(event.target)) {
+    menuOptions.style.display = "none";
+  }
+});
+
+// formulario de búsqueda
+
+
+
 // formulario de registro
 document.getElementById("registroForm").addEventListener("submit", function(event) {
 event.preventDefault();
@@ -23,21 +43,3 @@ alert(`✅ Registro exitoso\nBienvenido, ${nombre}`);
 // Aquí podrías enviar los datos al servidor con fetch()
 });
 
-
-// menú desplegable
-const menuToggle = document.getElementById("menuToggle");
-const menuOptions = document.getElementById("menuOptions");
-
-menuToggle.addEventListener("click", () => {
-  menuOptions.style.display =
-    menuOptions.style.display === "block" ? "none" : "block";
-});
-
-// Cierra el menú si haces clic fuera de él
-window.addEventListener("click", (event) => {
-  if (!menuToggle.contains(event.target) && !menuOptions.contains(event.target)) {
-    menuOptions.style.display = "none";
-  }
-});
-
-// formulario de búsqueda
