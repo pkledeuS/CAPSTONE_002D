0e071b3559923d59eed40c6644493865c85de254
pkledeuS <cristian.np99@gmail.com>
2025-10-13 20:39:30 -0300

    Se creó el nuevo home junto a su css

---
+++ fase 2/Evidencias Proyecto/mipc_ideal/app/static/js/carousel.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.carousel-2').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: true,
+    });
+});
